0bd047d066fc5294e8d03847f140789d2305315d
Johannes Krüger <me@johanneskr.de>
2021-02-01 21:22:08 +0100

    UnknownBot 2k21

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
 import logging
+import discord
+import configparser
+import sys
+config = configparser.ConfigParser()
+config.read('bin/config.ini')
+
+
+
+# //Logging - Müll
+class discord(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('[LOG] Message from {0.author}: {0.content}'.format(message))
+
+        if message.content.startswith('{0}help'.format(config['DISCORD']['PREFIX'])):
+            await message.channel.send('MOIN MEISTA')
+
+client = discord()
+client.run(config["DISCORD"]["TOKEN"])
